c2f085cf27197012e5977fb4b38d5ac23d236e91
oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-03-04 23:40:01 +0100

    add authentication logic to Home component

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,4 +1,8 @@
-function Home() {
+import { auth } from "./_lib/auth";
+
+async function Home() {
+  const session = await auth();
+  console.log(session);
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
